09edcbc420ebae3ec069df95a768f79e6edcc76f
xFA25E <55886566+xFA25E@users.noreply.github.com>
2019-10-17 21:11:39 +0200

    Better package

--- cyrillic-dvorak-im.el
+++ cyrillic-dvorak-im.el
@@ -1,3 +1,29 @@
+;;; cyrillic-dvorak-im.el --- Cyrillic-dvorak input method  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2019
+
+;; Author:  <>
+;; Keywords: convenience
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
 (require 'quail)
 
 (quail-define-package
@@ -109,3 +135,4 @@
  ("Z" ?,))
 
 (provide 'cyrillic-dvorak-im)
+;;; cyrillic-dvorak-im.el ends here
